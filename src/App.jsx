d7a0200a368e75ce8e61a10eb1c42f93a31c77fd
Computador Pampili <wellinton@null.net>
2025-08-29 16:12:16 -0300

    atualiza o layout do componente App para melhorar a apresentação e a responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ function App() {
 
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-56 bg-sky-200 text-white rounded-md p-4 ">
-        smartphone(640px)
+    <div className="from-violet-500 to-pink-600 bg-gradient-to-bl p-6 rounded-lg shadow-md mx-auto w-md- @container">
+      <p className="@max-sm:text-center @sm:bg-amber-300">
+        Container query, o futuro do CSS. Com container query, você pode aplicar estilos com base no tamanho do contêiner pai, em vez do tamanho da tela. Isso permite criar layouts mais flexíveis e adaptáveis, especialmente em componentes reutilizáveis.
+      </p>
+
+      <div className="w-full h-12 "></div>
+
+
+      <div className="relative size-16 mx-auto">
+        <span className="size-12 animate-ping bg-indigo-500 absolute inline-flex rounded-full opacity-75"></span>
+        <span className="size-16  bg-indigo-500 absolute inline-flex rounded-full opacity-75"></span>
       </div>
 
-      <div className="w-56 bg-sky-200 text-white rounded-md p-4 hidden md:block">
-        tablet(768px)
-      </div>
 
-      <div className="w-56 bg-sky-200 text-white rounded-md p-4 hidden lg:block">
-        laptop(1024px)
-      </div>
-
-      <div className="w-56 bg-sky-200 text-white rounded-md p-4 hidden xl:block">
-        desktop(1280px)
-      </div>
     </div>
   );
 }
