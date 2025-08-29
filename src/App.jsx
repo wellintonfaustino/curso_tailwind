1832ef7d943b81ad6c4dc6080d3ad63a167044bb
Computador Pampili <wellinton@null.net>
2025-08-29 11:15:34 -0300

    remove elementos não utilizados e reorganiza seções no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ function App() {
 
   return (
     <div className="space-y-4">
-      <div className="bg-purple-400 size-80 rounded-bl-full rounded-tr-full"> as</div>
-      <div className="w-full h-8 bg-purple-400"></div>
+      <section className="bg-amber-50 p-4 rounded shadow">
+        <h2 className="text-lg font-semibold">Section Title</h2>
+        <p className="text-gray-600">This is a description for the section.</p>
+      </section> 
+
+      <section className="bg-cyan-50 p-4 rounded shadow space-y-6">
+        
+          <h2 className="text-lg font-semibold">Another Section</h2>
+          <p className="text-gray-600  break-words truncate">This is a description for another section.</p>
+          <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse magni nihil sapiente facilis accusantium dolore! Architecto a at totam ipsum vero itaque explicabo, rerum corporis incidunt quis consequatur magni consequuntur.</p>
+          <p className="text-left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt sequi quo voluptates officiis, adipisci veritatis recusandae ipsa nostrum culpa, modi eligendi ab quasi corporis ducimus iure possimus tempora obcaecati quisquam!</p>
+          <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores facilis illo repellat rem ipsam fugit explicabo, laudantium ad totam quasi distinctio nesciunt, enim, eligendi aliquid nihil aliquam? Labore, unde quidem.</p>
+          <p className="text-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores facilis illo repellat rem ipsam fugit explicabo, laudantium ad totam quasi distinctio nesciunt, enim, eligendi aliquid nihil aliquam? Labore, unde quidem.</p>      
+      </section>
     </div>
   );
 }
