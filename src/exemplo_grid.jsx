27b2006d73973d3d7f010d3807400a7b1a93d45a
Computador Pampili <wellinton@null.net>
2025-08-29 14:49:24 -0300

    adiciona o componente Exemple com layout de grid e estilização inicial

---
+++ src/exemplo_grid.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Exemple() {
+
+
+  return (
+    <div className="w-[800px] min-h-[300px] mx-auto bg-sky-200 grid-cols-3 gap-4 p-4 grid">
+      <div className="w-full min-h-14 bg-red-200 col-span-3"> 1 </div>
+      <div className="w-full min-h-14 bg-red-200 col-span-2 row-span-3"> 2 </div>
+      <div className="w-full min-h-14 bg-red-200 "> 3 </div>
+      <div className="w-full min-h-14 bg-red-200 "> 4 </div>
+      <div className="w-full min-h-14 bg-red-200 col-span-3"> 5 </div>
+    </div>
+  );
+}
+
+export default Exemple;
